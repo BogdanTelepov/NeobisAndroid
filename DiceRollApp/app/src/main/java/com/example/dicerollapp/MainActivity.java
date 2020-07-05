2d0872e52f86b43a0e12c3b0931f5a1319535227
BogdanTelepov <mr.closer95@yandex.ru>
2020-07-05 20:56:21 +0600

    Dice Roll

--- DiceRollApp/app/src/main/java/com/example/dicerollapp/MainActivity.java
+++ DiceRollApp/app/src/main/java/com/example/dicerollapp/MainActivity.java
@@ -16,7 +16,10 @@ public class MainActivity extends AppCompatActivity {
     Button button;
 
     Random random = new Random();
-    int randomNumber;
+    int randomNumber1 = 0;
+    int randomNumber2 = 0;
+    static int firstDice = 0;
+    static int secondDice = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,62 +29,40 @@ public class MainActivity extends AppCompatActivity {
         imageView2 = findViewById(R.id.image2_view);
         button = findViewById(R.id.roll_button);
 
+        randomNumber1 = random.nextInt(6) + 1;
+        randomNumber2 = random.nextInt(6) + 1;
+        if (firstDice == 0 && secondDice == 0) {
+            firstDice = getResources().getIdentifier("dice" + randomNumber1, "drawable", "com.example.dicerollapp");
+            secondDice = getResources().getIdentifier("dice" + randomNumber2, "drawable", "com.example.dicerollapp");
+        }
+        imageView1.setImageResource(firstDice);
+        imageView2.setImageResource(secondDice);
+
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                rollForFirstImage();
-                rollForSecondImage();
+                randomNumber1 = random.nextInt(6) + 1;
+                randomNumber2 = random.nextInt(6) + 1;
+                firstDice = getResources().getIdentifier("dice" + randomNumber1, "drawable", "com.example.dicerollapp");
+                secondDice = getResources().getIdentifier("dice" + randomNumber2, "drawable", "com.example.dicerollapp");
+
+                imageView1.setImageResource(firstDice);
+                imageView2.setImageResource(secondDice);
             }
         });
     }
 
+    protected void onSaveInstanceState(Bundle outState) {
 
-    private void rollForFirstImage() {
-        randomNumber = random.nextInt(6) + 1;
-        switch (randomNumber) {
-            case 1:
-                imageView1.setImageResource(R.drawable.dice1);
-                break;
-            case 2:
-                imageView1.setImageResource(R.drawable.dice2);
-                break;
-            case 3:
-                imageView1.setImageResource(R.drawable.dice3);
-                break;
-            case 4:
-                imageView1.setImageResource(R.drawable.dice4);
-                break;
-            case 5:
-                imageView1.setImageResource(R.drawable.dice5);
-                break;
-            case 6:
-                imageView1.setImageResource(R.drawable.dice6);
-                break;
-        }
+        super.onSaveInstanceState(outState);
+        outState.putInt("image1_view", firstDice);
+        outState.putInt("image2_view", secondDice);
     }
 
-    private void rollForSecondImage() {
-        randomNumber = random.nextInt(6) + 1;
-        switch (randomNumber) {
-            case 1:
-                imageView2.setImageResource(R.drawable.dice1);
-                break;
-            case 2:
-                imageView2.setImageResource(R.drawable.dice2);
-                break;
-            case 3:
-                imageView2.setImageResource(R.drawable.dice3);
-                break;
-            case 4:
-                imageView2.setImageResource(R.drawable.dice4);
-                break;
-            case 5:
-                imageView2.setImageResource(R.drawable.dice5);
-                break;
-            case 6:
-                imageView2.setImageResource(R.drawable.dice6);
-                break;
-        }
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        firstDice = savedInstanceState.getInt("image1_view");
+        secondDice = savedInstanceState.getInt("image2_view");
     }
 
 
